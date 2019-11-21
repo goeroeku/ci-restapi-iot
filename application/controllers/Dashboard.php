212a04cc0d3fd9d5766e9d2b28f10e85009bc670
goeroeku <goeroeku@gmail.com>
2019-11-21 07:11:45 +0700

    fix dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -4,8 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Dashboard extends CI_Controller {
     public function index()
 	{
-		$this->load->helper('url');
-
-		$this->load->view('welcome_message');
+		$this->load->view('dashboard.index');
 	}
 }
